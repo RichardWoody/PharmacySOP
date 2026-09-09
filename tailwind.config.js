738352681e9697f64a5f1b1ed4d35409c5786fa4
RichardWoody <rwoodville@hotmail.com>
2026-09-09 02:10:04 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nhs: {
+          blue: '#005EB8',
+          darkblue: '#003087',
+          brightblue: '#0072CE',
+          lightblue: '#41B6E6',
+          aqua: '#00A9CE',
+          green: '#007F3B',
+          warmyellow: '#FFB81C',
+          darkpink: '#7C2855',
+          darkgrey: '#425563',
+          midgrey: '#768692',
+          paleblue: '#E8EDEE',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
